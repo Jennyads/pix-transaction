b9f5894ed6af3f4457fa157bc0b9a1076b99bd2c
Victor Cruz <victor.cruz@vocedm.com.br>
2024-02-16 18:06:45 -0300

    feat: add dockerfiles

--- profile/cmd/main.go
+++ profile/cmd/main.go
@@ -23,7 +23,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	config, err := cfg.Load()
--- transaction/cmd/main.go
+++ transaction/cmd/main.go
@@ -20,7 +20,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	config, err := cfg.Load()
